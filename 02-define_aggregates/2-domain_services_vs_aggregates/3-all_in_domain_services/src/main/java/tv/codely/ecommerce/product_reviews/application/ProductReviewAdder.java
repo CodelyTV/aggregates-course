713dbd633cb09b542af6f962057c2db0f70b3cd5
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-11 18:30:13 +0200

    feat(02-2): add all in domain services

--- 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/application/ProductReviewAdder.java
+++ 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/application/ProductReviewAdder.java
@@ -1,24 +1,16 @@
 package tv.codely.ecommerce.product_reviews.application;
 
 import tv.codely.ecommerce.product_reviews.domain.ProductReview;
-import tv.codely.ecommerce.product_reviews.domain.ProductReviewCommentEnsurer;
-import tv.codely.ecommerce.product_reviews.domain.ProductReviewRatingEnsurer;
+import tv.codely.ecommerce.product_reviews.domain.ProductReviewEnsurer;
 import tv.codely.ecommerce.product_reviews.domain.ProductReviewRepository;
 import tv.codely.ecommerce.products.application.ProductFinder;
 
 record ProductReviewAdder(ProductReviewRepository repository, ProductFinder finder) {
     public void add(String id, String productId, String userId, Integer rating, String comment) {
-        ProductReviewRatingEnsurer.ensure(rating);
-        ProductReviewCommentEnsurer.ensure(comment);
-
-        ensureProductExist(productId);
+        ProductReviewEnsurer.ensure(rating, comment, finder, productId);
 
         var review = ProductReview.create(id, productId, userId, rating, comment);
 
         this.repository.save(review);
-    }
-
-    private void ensureProductExist(String productId) {
-        this.finder.find(productId);
     }
 }

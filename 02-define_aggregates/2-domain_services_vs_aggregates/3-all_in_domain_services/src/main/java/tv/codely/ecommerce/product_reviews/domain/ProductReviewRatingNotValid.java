050084f476b0ab9b8135d433440cb290786bcd92
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-11 18:52:11 +0200

    feat(02-2): test commit to check script to see files that change together

--- 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewRatingNotValid.java
+++ 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewRatingNotValid.java
@@ -2,6 +2,6 @@ package tv.codely.ecommerce.product_reviews.domain;
 
 public class ProductReviewRatingNotValid extends RuntimeException {
     public ProductReviewRatingNotValid(Integer rating) {
-        super("The review rating <" + rating + "> is not valid. It must be between 0 and 10");
+        super("The review rating <" + rating + "> is not valid. It must be between 0 and 5");
     }
 }

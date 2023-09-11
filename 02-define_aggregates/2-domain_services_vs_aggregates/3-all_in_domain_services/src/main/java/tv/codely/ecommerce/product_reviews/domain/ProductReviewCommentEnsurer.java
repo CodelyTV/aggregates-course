55577746983720c0db2f2c76aa6afc400c03c550
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-11 18:53:22 +0200

    feat(02-2): test commit to check script to see files that change together

--- 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewCommentEnsurer.java
+++ 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewCommentEnsurer.java
@@ -2,7 +2,7 @@ package tv.codely.ecommerce.product_reviews.domain;
 
 public class ProductReviewCommentEnsurer {
     public static void ensure(String comment) {
-        if (comment.length() > 500) {
+        if (comment.length() > 550) {
             throw new ProductReviewCommentIsTooLong(comment);
         }
     }

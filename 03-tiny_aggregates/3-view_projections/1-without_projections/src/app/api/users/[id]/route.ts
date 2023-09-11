2f303cfe4f64401c98dd9ab372e1120d3ec1030c
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-12 00:25:18 +0200

    feat(3-3): create and get users

--- 03-tiny_aggregates/3-view_projections/1-without_projections/src/app/api/users/[id]/route.ts
+++ 03-tiny_aggregates/3-view_projections/1-without_projections/src/app/api/users/[id]/route.ts
@@ -3,13 +3,13 @@ import * as t from "io-ts";
 import { PathReporter } from "io-ts/PathReporter";
 import { NextRequest, NextResponse } from "next/server";
 
-import { MariaDBConnection } from "../../../../modules/shared/infrastructure/MariaDBConnection";
-import { UserRegistrar } from "../../../../modules/users/application/create/UserRegistrar";
-import { UserSearcher } from "../../../../modules/users/application/search/UserSearcher";
-import { UserPrimitives } from "../../../../modules/users/domain/User";
-import { MySqlUserRepository } from "../../../../modules/users/infrastructure/MySqlUserRepository";
+import { MariaDBConnection } from "../../../../contexts/shared/infrastructure/MariaDBConnection";
+import { UserRegistrar } from "../../../../contexts/shop/users/application/create/UserRegistrar";
+import { UserSearcher } from "../../../../contexts/shop/users/application/search/UserSearcher";
+import { UserPrimitives } from "../../../../contexts/shop/users/domain/User";
+import { MySqlUserRepository } from "../../../../contexts/shop/users/infrastructure/MySqlUserRepository";
 
-const CreateUserRequest = t.type({ name: t.string });
+const CreateUserRequest = t.type({ name: t.string, profilePicture: t.string });
 
 export async function PUT(
 	request: NextRequest,
@@ -25,7 +25,11 @@ export async function PUT(
 
 	const body = validatedRequest.right;
 
-	await new UserRegistrar(new MySqlUserRepository(new MariaDBConnection())).create(id, body.name);
+	await new UserRegistrar(new MySqlUserRepository(new MariaDBConnection())).create(
+		id,
+		body.name,
+		body.profilePicture,
+	);
 
 	return new Response("", { status: 201 });
 }
--- 03-tiny_aggregates/3-view_projections/1-without_projections/src/contexts/shop/users/application/create/UserRegistrar.ts
+++ 03-tiny_aggregates/3-view_projections/1-without_projections/src/contexts/shop/users/application/create/UserRegistrar.ts
@@ -4,8 +4,8 @@ import { UserRepository } from "../../domain/UserRepository";
 export class UserRegistrar {
 	constructor(private readonly repository: UserRepository) {}
 
-	async create(id: string, name: string): Promise<void> {
-		const product = User.create(id, name);
+	async create(id: string, name: string, profilePicture: string): Promise<void> {
+		const product = User.create(id, name, profilePicture);
 
 		await this.repository.save(product);
 	}

fbbc0263ded843036878adbf97d7504aa3ffd180
Rafa Gómez <rgomezcasas@gmail.com>
2023-09-11 18:55:02 +0200

    feat(02-2): test commit to check script to see files that change together

--- 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewRatingEnsurer.java
+++ 02-define_aggregates/2-domain_services_vs_aggregates/3-all_in_domain_services/src/main/java/tv/codely/ecommerce/product_reviews/domain/ProductReviewRatingEnsurer.java
@@ -2,7 +2,7 @@ package tv.codely.ecommerce.product_reviews.domain;
 
 public class ProductReviewRatingEnsurer {
     public static void ensure(Integer rating) {
-        if (rating < 0 || rating > 10) {
+        if (rating < 0 || rating > 5) {
             throw new ProductReviewRatingNotValid(rating);
         }
     }

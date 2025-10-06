9da084925f9c574bfb3f497ade2d3757712cf64f
Rafa Gómez <rgomezcasas@gmail.com>
2025-10-06 23:15:25 +0200

    feat(06-1): add example

--- 06-common_problems/4-ids_from_outside/tests/contexts/mooc/invoices/infrastructure/PostgresInvoiceRepository.test.ts
+++ 06-common_problems/4-ids_from_outside/tests/contexts/mooc/invoices/infrastructure/PostgresInvoiceRepository.test.ts
@@ -34,63 +34,79 @@ describe("PostgresInvoiceRepository should", () => {
 		});
 
 		it("return 2 when serie already has invoice with number 1", async () => {
-			const serie = "A";
-			const invoice = InvoiceMother.create({ serie, number: 1 });
+			const serie = InvoiceSerieMother.create();
+			const invoice = InvoiceMother.create({
+				serie: serie.value,
+				number: 1,
+			});
 			await repository.save(invoice);
 
-			const nextNumber = await repository.nextNumber(serie);
+			const nextNumber = await repository.nextNumber(serie.value);
 
 			expect(nextNumber).toBe(2);
 		});
 
 		it("return correct next number when multiple invoices exist for the same serie", async () => {
-			const serie = "B";
-			const invoice1 = InvoiceMother.create({ serie, number: 1 });
-			const invoice2 = InvoiceMother.create({ serie, number: 2 });
-			const invoice3 = InvoiceMother.create({ serie, number: 3 });
+			const serie = InvoiceSerieMother.create();
+			const invoice1 = InvoiceMother.create({
+				serie: serie.value,
+				number: 1,
+			});
+			const invoice2 = InvoiceMother.create({
+				serie: serie.value,
+				number: 2,
+			});
+			const invoice3 = InvoiceMother.create({
+				serie: serie.value,
+				number: 3,
+			});
 
 			await repository.save(invoice1);
 			await repository.save(invoice2);
 			await repository.save(invoice3);
 
-			const nextNumber = await repository.nextNumber(serie);
+			const nextNumber = await repository.nextNumber(serie.value);
 
 			expect(nextNumber).toBe(4);
 		});
 
 		it("return 1 for a serie when other series have invoices", async () => {
-			const invoiceSerieA = InvoiceMother.create({ serie: "A" });
-			const invoiceSerieB = InvoiceMother.create({ serie: "B" });
+			const serieA = InvoiceSerieMother.create();
+			const serieB = InvoiceSerieMother.create();
+			const serieC = InvoiceSerieMother.create();
+
+			const invoiceSerieA = InvoiceMother.create({ serie: serieA.value });
+			const invoiceSerieB = InvoiceMother.create({ serie: serieB.value });
 
 			await repository.save(invoiceSerieA);
 			await repository.save(invoiceSerieB);
 
-			const nextNumber = await repository.nextNumber("C");
+			const nextNumber = await repository.nextNumber(serieC.value);
 
 			expect(nextNumber).toBe(1);
 		});
 
 		it("not be affected by other series numbers", async () => {
-			const serieA = "A";
-			const serieB = "B";
+			const serieA = InvoiceSerieMother.create();
+			const serieB = InvoiceSerieMother.create();
 			const invoiceSerieA1 = InvoiceMother.create({
-				serie: serieA,
+				serie: serieA.value,
 				number: 1,
 			});
 			const invoiceSerieA2 = InvoiceMother.create({
-				serie: serieA,
+				serie: serieA.value,
 				number: 2,
 			});
 			const invoiceSerieB1 = InvoiceMother.create({
-				serie: serieB,
+				serie: serieB.value,
 				number: 1,
 			});
 			const invoiceSerieB2 = InvoiceMother.create({
-				serie: serieB,
+				serie: serieB.value,
 				number: 2,
 			});
 			const invoiceSerieB3 = InvoiceMother.create({
-				serie: serieB,
+				serie: serieB.value,
 				number: 3,
 			});
 
@@ -100,8 +116,8 @@ describe("PostgresInvoiceRepository should", () => {
 			await repository.save(invoiceSerieB2);
 			await repository.save(invoiceSerieB3);
 
-			const nextNumberA = await repository.nextNumber(serieA);
-			const nextNumberB = await repository.nextNumber(serieB);
+			const nextNumberA = await repository.nextNumber(serieA.value);
+			const nextNumberB = await repository.nextNumber(serieB.value);
 
 			expect(nextNumberA).toBe(3);
 			expect(nextNumberB).toBe(4);
